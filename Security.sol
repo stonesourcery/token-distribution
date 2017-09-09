a03b55d7c58f42257d830928c41e561892d4703a
Steven Stewart <steven.stewart@uwaterloo.ca>
2017-09-09 18:01:46 -0400

    QSP-9: Some edits to comments

--- Security.sol
+++ Security.sol
@@ -1,21 +1,27 @@
 pragma solidity ^0.4.16;
 
+/**
+ * An ownable contract has an owner and the ownership can be transferred.
+ * The "onlyOwner" modifier can be used to constrain access to a function
+ * to the owner.
+ */
 contract Ownable
 {
   address public owner;
 
   /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
+   * The Ownable constructor sets the original `owner` of the
+   * contract to the sender account.
    */
   function Ownable()
   {
+    // whoever instantiates this contract is automatically made the owner
     owner = msg.sender;
   }
 
 
   /**
-   * @dev Throws if called by any account other than the owner.
+   * Throws if called by any account other than the owner.
    */
   modifier onlyOwner()
   {
@@ -25,7 +31,8 @@ contract Ownable
 
 
   /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * Allows the current owner to transfer control of the contract to a newOwner.
+   *
    * @param newOwner The address to transfer ownership to.
    */
   function transferOwnership(address newOwner) onlyOwner {
