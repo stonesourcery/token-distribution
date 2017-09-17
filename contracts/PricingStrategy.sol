063f28680e8f08db114785edadff6b69b544981f
ezulkosk <ezulkosk@gsd.uwaterloo.ca>
2017-09-16 23:35:17 -0400

    QSP-47: merge

--- contracts/PricingStrategy.sol
+++ contracts/PricingStrategy.sol
@@ -3,8 +3,6 @@ pragma solidity ^0.4.15;
 
 import "./SafeMathLib.sol";
 
-<<<<<<< HEAD
-
 library PricingStrategy
 {
     using SafeMathLib for *;
@@ -13,33 +11,10 @@ library PricingStrategy
     struct PricingStrategyInfo
     {
         uint multiplier; // the amount of tokens per unit of Ether
-=======
-library PricingStrategy
-{
-    struct PricingStrategyInfo
-    {
-        // Tiered bonuses
-        uint[] tierBonusAmount;
-        uint[] tierBonusReward;
-        uint tierBonusCount;
     }
 
 
     /**
-    * Update the pricing strategy with new parameters.
-    * TODO: need to check the permissions on this
-    */
-    function updatePricingStrategy(PricingStrategyInfo storage self, uint[] amounts, uint[] rewards, uint count)
-    {
-        self.tierBonusAmount = amounts;
-        self.tierBonusReward = rewards;
-        self.tierBonusCount = count;
->>>>>>> d196aafe5abba181ae341abcbad1f9a722294d99
-    }
-
-
-    /**
-<<<<<<< HEAD
      * Returns the number of tokens to be reward based on the supplied
      * pricing strategy and amount of Ether contributed.
      */
@@ -47,33 +22,5 @@ library PricingStrategy
     {
         return amountInEther.times(info.multiplier);
     }
-=======
-     * Given the specified amount of tokens, this function determines the amount
-     * of bonus tokens that may be awarded based on current pricing strategy.
-     *
-     * The pricing strategy data structure makes no guarantees that the tier-related
-     * data are sorted, so this function must step through all tiers to determine
-     * the maximum bonus for the specified amount of tokens.
-     */
-    function getTokenBonusAmount(PricingStrategyInfo storage self, uint amountInTokens) returns(uint)
-    {
-        uint maxBonus = 0;
-        uint count = self.tierBonusCount;
 
-        for (uint i = 0; i < count; i++)
-        {
-            if (amountInTokens >= self.tierBonusAmount[i])
-            {
-                uint bonus = self.tierBonusReward[i];
-                if (maxBonus < bonus)
-                {
-                    maxBonus = bonus;
-                }
-            }
-        }
-
-        return amountInTokens * (1 + maxBonus / 100);
-    }
-
->>>>>>> d196aafe5abba181ae341abcbad1f9a722294d99
 }
--- migrations/2_crowdfund_migration.js
+++ migrations/2_crowdfund_migration.js
@@ -2,13 +2,9 @@ var ERC20Lib = artifacts.require("./ERC20Lib.sol");
 var SafeMathLib = artifacts.require("./SafeMathLib.sol");
 var StandardToken = artifacts.require("./StandardToken.sol");
 
-<<<<<<< HEAD
 var Ownable = artifacts.require("./Ownable.sol");
 var Pausable = artifacts.require("./Pausable.sol");
-=======
-var Ownable = artifacts.require("Ownable");
-var Pausable = artifacts.require("Pausable");
->>>>>>> d196aafe5abba181ae341abcbad1f9a722294d99
+
 
 var PricingStrategy = artifacts.require("./PricingStrategy.sol");
 var Quantstamp = artifacts.require("./Quantstamp.sol");
@@ -17,10 +13,7 @@ module.exports = function(deployer) {
     deployer.deploy(SafeMathLib);
 
     deployer.deploy(Ownable);
-<<<<<<< HEAD
     deployer.link(Ownable, Pausable);
-=======
->>>>>>> d196aafe5abba181ae341abcbad1f9a722294d99
     deployer.deploy(Pausable);
 
     deployer.link(SafeMathLib, ERC20Lib);
@@ -37,13 +30,9 @@ module.exports = function(deployer) {
     deployer.link(SafeMathLib, Quantstamp);
     deployer.link(PricingStrategy, Quantstamp);
     deployer.link(StandardToken, Quantstamp);
-<<<<<<< HEAD
     deployer.link(Ownable, Quantstamp);
     deployer.link(Pausable, Quantstamp);
 
     //deployer.deploy(Quantstamp);
     deployer.deploy(Quantstamp, 0x7e5f4552091a69125d5dfcb7b8c2659029395bdf, 1000, 100000, 60);
-=======
-    deployer.deploy(Quantstamp);
->>>>>>> d196aafe5abba181ae341abcbad1f9a722294d99
 };
